88120bf0b3a6169089e887216b70a9f4c91ee0d1
Jorge Martinez <hmartinezpu@gmail.com>
2019-06-22 15:41:34 -0500

    Cors

--- ANgular 7/auth/authcontroller.js
+++ ANgular 7/auth/authcontroller.js
@@ -4,48 +4,60 @@ const bcrypt = require('bcryptjs');
 
 const SECRET_KEY='1234';
 
-exports.createUser = (req, res, next)=>{
+exports.createUser = (req, res, next) => {
     const newUser = {
-        name: req.body.name,
-        email: req.body.email,
-        pass: req.body.pass,
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password)
     }
-    User.create(newUser,(err,user)=>{
-        if (err) return res.status(500).send('errorserver');
-        const expiresIn = 24*60*60;
-        const accessToken = jwt.sign({id: user.id},
-            SECRET_KEY,{
-                expiresIn:expiresIn
-            });
-     //respuesta
-    res.send({user});
-});
-}
-exports.loginUser= (req, res , next) =>{
-
-    const userData = {
-        email: req.body.email,
-        password: req.body.password
-    }
-    User.findOne({email: userData.email}, (err, user)=>{
-        if (err) return res.status(500).send('Server error!');
-        if (!user){
-            res.status(409).send({message: ' error!'});
-        }else {
-            const resultpass = userData.password;
-            if(resultpass){
-                const expiresIn = 24*60*60;
-                const accessToken = jwt.sign({id: user.id},
-                 SECRET_KEY,{
-                expiresIn:expiresIn
-            });
-            //respuesta
-            res.send({user});
-            }else {
-                res.status(409).send({message: ' error!'}); 
-            }
-        }
+  
+    User.create(newUser, (err, user) => {
+      if (err && err.code === 11000) return res.status(409).send('Email already exists');
+      if (err) return res.status(500).send('Server error');
+      const expiresIn = 24 * 60 * 60;
+      const accessToken = jwt.sign({ id: user.id },
+        SECRET_KEY, {
+          expiresIn: expiresIn
+        });
+      const dataUser = {
+        name: user.name,
+        email: user.email,
+        accessToken: accessToken,
+        expiresIn: expiresIn
+      }
+      // response 
+      res.send({ dataUser });
     });
-
-
-}
+  }
+  
+  exports.loginUser = (req, res, next) => {
+    const userData = {
+      email: req.body.email,
+      password: req.body.password
+    }
+    User.findOne({ email: userData.email }, (err, user) => {
+      if (err) return res.status(500).send('Server error!');
+  
+      if (!user) {
+        // email does not exist
+        res.status(409).send({ message: 'Something is wrong' });
+      } else {
+        const resultPassword = bcrypt.compareSync(userData.password, user.password);
+        if (resultPassword) {
+          const expiresIn = 24 * 60 * 60;
+          const accessToken = jwt.sign({ id: user.id }, SECRET_KEY, { expiresIn: expiresIn });
+  
+          const dataUser = {
+            name: user.name,
+            email: user.email,
+            accessToken: accessToken,
+            expiresIn: expiresIn
+          }
+          res.send({ dataUser });
+        } else {
+          // password wrong
+          res.status(409).send({ message: 'Something is wrong' });
+        }
+      }
+    });
+  }
--- ANgular 7/server.js
+++ ANgular 7/server.js
@@ -2,11 +2,19 @@
 const express = require('express');
 const authRoutes = require ('./auth/authrutes')
 const app = express();
+const cors = require('cors');
 const propierties = require('./config/properties');
 
 const router = express.Router();
 const DB = require('./config/db');
 //se inicia la bd
 DB();
+const bodyParser = require('body-parser');
+const bodyParserJSON = bodyParser.json();
+const bodyParserURLEncoded = bodyParser.urlencoded({ extended: true });
+
+app.use(bodyParserJSON);
+app.use(bodyParserURLEncoded);
+app.use(cors());
 
 app.listen(3000, ()=>console.log('server running'));
